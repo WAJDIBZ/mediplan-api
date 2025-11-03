4d7caa468409fb429210afe31f0b20fd285643f0
WAJDIBZ <benzinebwajdi@rades.r-iset.tn>
2025-11-03 15:09:17 +0100

    Initial commit

---
+++ src/main/java/com/sirm/mediplanapi/MediplanApiApplication.java
@@ -0,0 +1,13 @@
+package com.sirm.mediplanapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediplanApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediplanApiApplication.class, args);
+    }
+
+}
