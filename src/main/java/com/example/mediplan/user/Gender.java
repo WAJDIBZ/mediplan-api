8c9a7ea7bd8ac632793d51f0f164ffb1838389d5
WAJDIBZ <benzinebwajdi@rades.r-iset.tn>
2025-11-07 13:08:30 +0100

    ADMINfix

--- src/main/java/com/example/mediplan/user/Gender.java
+++ src/main/java/com/example/mediplan/user/Gender.java
@@ -1,8 +1,33 @@
 package com.example.mediplan.user;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 
 public enum Gender {
-    MALE,
-    FEMALE,
-    OTHER,
-    UNDISCLOSED
+    MALE("HOMME"),
+    FEMALE("FEMME"),
+    OTHER("AUTRE"),
+    UNDISCLOSED("NON_PRÉCISÉ");
+
+    private final String displayName;
+
+    Gender(String displayName) {
+        this.displayName = displayName;
+    }
+
+    @JsonCreator
+    public static Gender fromString(String value) {
+        for (Gender g : Gender.values()) {
+            if (g.name().equalsIgnoreCase(value) ||
+                    g.displayName.equalsIgnoreCase(value)) {
+                return g;
+            }
+        }
+        throw new IllegalArgumentException("Unknown gender: " + value);
+    }
+
+    @JsonValue
+    public String getDisplayName() {
+        return displayName;
+    }
 }
