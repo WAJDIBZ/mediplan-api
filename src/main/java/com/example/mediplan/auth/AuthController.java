65d0351b66b552f522409c298f208c46201f791f
WAJDIBZ <benzinebwajdi@rades.r-iset.tn>
2025-11-07 12:22:51 +0100

    ADMINfix

--- src/main/java/com/example/mediplan/auth/AuthController.java
+++ src/main/java/com/example/mediplan/auth/AuthController.java
@@ -11,17 +11,12 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
 @Tag(name = "Authentification")
+@CrossOrigin
 public class AuthController {
 
     private final AuthService authService;
